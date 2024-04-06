3cc6c10a2ee63594c250e4a46a1364b02b780e8d
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-06 12:40:37 +0000

    Works!

--- githubList.py
+++ githubList.py
@@ -4,15 +4,6 @@
 Poyo;       - 
 Silver;     - we are going to have to make it get the file that contains all the urls from github if that's how you want it to find the urls
 
-got this code from a google search:
-
-
-import urllib2  # the lib that handles the url stuff
-
-data = urllib2.urlopen(target_url) # it's a file like object and works just like a file
-for line in data: # files are iterable
-    print line
-oooo awesome!
 
 
 TODO: 
@@ -25,19 +16,27 @@ from urllib.request import urlopen
 
 response = urlopen('https://raw.githubusercontent.com/ucanet/ucanet-registry/main/ucanet-registry.txt')
 
+print("response", response) #actually just see what it's giving us, incase of errors
+
+
+
 def update_list(current_diectory, next_directory):
     first_words = get_first_word()
     return first_words
 
-lines = [""]
+
+
 
 # Get first word of each line
 def get_first_word(lines):
+    # for line in lines:
+    #     print(line)
     return [line.split()[0] for line in lines]
 
+
 # Execute the function
-first_words = get_first_word(lines)
+first_words = get_first_word(response)
 
 # Output
 for word in first_words:
-    print(word)
+    print(str(word)[2:-1])
