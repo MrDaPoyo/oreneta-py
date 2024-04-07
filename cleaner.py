06edff6abd6dc8df633de2d6a37dfbcfbaf8641a
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-07 17:32:12 +0000

    Cleaner.py CheckURL function working!

--- cleaner.py
+++ cleaner.py
@@ -1,20 +1,12 @@
 import requests
-import ipaddress
 
-def checkURL(url):
-    x = requests.get("http://", url)
+def checkURL(url): # check if website is up and running by URL
+    x = requests.get("http://" + url)
     print(x.status_code)
 
-    if int(str(x.status_code)[:2]) == 2:
+    if int(str(x.status_code)[:(int(len(str(x.status_code)))-1)]) == 2:
         return True
     else:
         return False
 
-def checkIP(ip):
-    try:
-        ip_object = ipaddress.ip_address(ip)
-        return True
-    except:
-        return False
-
-
+checkURL("wikipedia.com")
