928650294da47e87a05782e30db0277d5b0d024e
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-07 17:47:44 +0000

    Added Debugging

--- cleaner.py
+++ cleaner.py
@@ -5,12 +5,14 @@ def checkURL(url, HTTPS=False): # check if website is up and running by URL
         x = requests.get("https://" + url)
     else:
         x = requests.get("http://" + url)
-    print(x.status_code)
+    print("Status code: " + str(x.status_code))
     if int(str(x.status_code)[:1]) == 2:
+        print("OK")
         return True
     else:
+        print("NOK")
         return False
 
 
 # Test --> OK
-print(checkURL("wikipedia.org"))
+checkURL("wikipedia.org")
