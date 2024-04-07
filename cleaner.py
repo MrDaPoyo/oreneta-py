ac59649d51286a5544017a0c715af15cfdd629af
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-07 18:06:01 +0000

    Added exception handling

--- cleaner.py
+++ cleaner.py
@@ -1,18 +1,29 @@
 import requests
 
 def checkURL(url, HTTPS=False): # check if website is up and running by URL
-    if HTTPS == True:
-        x = requests.get("https://" + url)
-    else:
-        x = requests.get("http://" + url)
-    print("Status code: " + str(x.status_code))
-    if int(str(x.status_code)[:1]) == 2:
-        print("OK")
-        return True
-    else:
-        print("NOK")
-        return False
+    
+    try:
+        if HTTPS == True:
+            x = requests.get("https://" + url)
+        else:
+            x = requests.get("http://" + url)
+        print("Status code: " + str(x.status_code))
+
+        if int(str(x.status_code)[:1]) == 2:
+            print("OK")
+            return True
+        else:
+            print("NOK")
+            return False
+    except Exception:
+        
+        print(f"ERR " + str(Exception))
+        return "ERR"
 
 
 # Test --> OK
 checkURL("wikipedia.org")
+# Test --> NOK
+checkURL("skibidi.org")
+# Test --> ERR
+checkURL("rosesareviolet.org")
