b827c80f68a9566654b8c156a61d0ae7ad7b1591
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-07 17:33:21 +0000

    added option to use http or https checkURL()

--- cleaner.py
+++ cleaner.py
@@ -1,7 +1,10 @@
 import requests
 
-def checkURL(url): # check if website is up and running by URL
-    x = requests.get("http://" + url)
+def checkURL(url, HTTPS=False): # check if website is up and running by URL
+    if HTTPS == True:
+        x = requests.get("https://" + url)
+    else:
+        x = requests.get("http://" + url)
     print(x.status_code)
 
     if int(str(x.status_code)[:(int(len(str(x.status_code)))-1)]) == 2:
