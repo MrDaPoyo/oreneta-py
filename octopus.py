1765ee90415c116fbc05fcb7ad1ce07354f352d5
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-09 11:03:41 +0000

    OctoctoPus!

--- octopus.py
+++ octopus.py
@@ -4,4 +4,6 @@ print("I'm an octopus! =;O;=")
 time.sleep(2)
 x = input("Are you an octopus? =;v;=")
 if "y" in x:
-    print("I am too! \;W;/")
+    print("Me too! \;u;/")
+else:
+    print("Aww =;n;=")
