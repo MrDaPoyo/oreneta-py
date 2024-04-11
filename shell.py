887633942515454da3f37982657d0b836e97f55c
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-11 10:51:55 +0000

    shell 2.0

--- shell.py
+++ shell.py
@@ -1,18 +1,20 @@
 from cleaner import neocities
 
-function_dict = {"neocities":neocities}
+function_dict = {"neocities":neocities,"syntax":print("Syntax: [command] [arg1] NOTE: Ignore the brackets '[]'")}
 
 while True:
     INPUT = str(input("COMMAND: "))
-    if INPUT == "syntax":
-        print("Syntax: [command] [arg1] NOTE: Ignore the brackets '[]'")
-    else:
+    k = len(INPUT.split())
+    print(k)
+    if k != 1:
         command = INPUT.split(' ')[0]
         print(command) 
         arg1 = INPUT.split(' ')[1]
         print(arg1)
-        if command in function_dict:
-            response = function_dict[command](str(arg1))
-            print(str(response))
-        else:
-            print("INVALID")
+    else:
+        command = str(INPUT.split())
+    if command in str(function_dict):
+        response = function_dict[command](str(arg1))
+        print(str(response))
+    else:
+        print("INVALID")
