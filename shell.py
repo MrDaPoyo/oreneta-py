e9ae55af57666a1f617a8e3c643a9b9ba1bfaf05
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-11 07:25:27 +0000

    shell.py

--- shell.py
+++ shell.py
@@ -1,8 +1,15 @@
-command_list = ["read"]
+from cleaner import neocities
+
+function_dict = {"neocities":neocities}
 
 while True:
     INPUT = str(input("COMMAND: "))
-    if INPUT in command_list:
-        print(INPUT)
+    command = INPUT.split(' ', 1)[0]
+    print(command) 
+    arg1 = INPUT.split(' ', 2)[0]
+    print(arg1)
+    if command in function_dict:
+        response = function_dict[command](arg1)
+        print(str(response))
     else:
         print("INVALID")
